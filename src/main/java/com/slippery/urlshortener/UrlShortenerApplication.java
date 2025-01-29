c906ecc2315f14eb98ca0f50bcaa19198ddbc618
johnkinuthiaa <kinuthiajohn122@gmail.com>
2025-01-30 01:49:43 +0300

    caching

--- src/main/java/com/slippery/urlshortener/UrlShortenerApplication.java
+++ src/main/java/com/slippery/urlshortener/UrlShortenerApplication.java
@@ -2,8 +2,11 @@ package com.slippery.urlshortener;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cache.annotation.EnableCaching;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
 @SpringBootApplication
+@EnableCaching
 public class UrlShortenerApplication {
 
     public static void main(String[] args) {
